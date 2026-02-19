7e95db2a52b08936671a913c87fb006b34237f31
Topseen-a <abodunrintopseen@gmail.com>
2026-02-19 08:51:01 +0100

    semicolon

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,31 +1,36 @@
 package tictactoe;
 
+import java.sql.SQLOutput;
+
 public class TicTacToe {
     public static void main(String[] args) {
 
-        int[][] game = new int[3][3];
-        display(game);
+        String[][] gameOne = {
+                {"x", "o", "x"},
+                {"o", "x", "o"},
+                {"x", "o", "x"}
+        };
 
-        char player = 'x';
-        player = alternate(player);
-        System.out.println("Next player: " + player);
-    }
-
-    public static void display(int[][] array) {
-        for (int row = 0; row < array.length; row++) {
-            for (int column = 0; column < array[row].length; column++) {
-                System.out.print("| " + array[row][column] + " ");
+        for (int row = 0; row < gameOne.length; row++) {
+            for (int column = 0; column < gameOne[row].length; column++) {
+                System.out.print(gameOne[row][column]);
             }
-            System.out.println("|");
+            System.out.println(" ");
         }
-    }
+        System.out.println();
 
-    public static char alternate(char currentPlay) {
-        if (currentPlay == 'x') {
-            return 'o';
+        int [][] gameTwo = {
+                {1, 0, 1},
+                {0, 1, 0},
+                {1, 0, 1}
+        };
+
+        for (int row = 0; row < gameTwo.length; row++) {
+            for (int column = 0; column < gameTwo[row].length; column++) {
+                System.out.print(gameTwo[row][column]);
+            }
+            System.out.println(" ");
         }
-        else {
-            return 'x';
-        }
+        System.out.println();
     }
 }
